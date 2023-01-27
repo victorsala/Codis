9f03cb4a7bfc54a5f365fe68faced7e4f65685b6
victorsala <victor.sala@gmail.com>
2023-01-27 19:56:56 +0100

    ModifiedByChat

--- ChatGPT.js
+++ ChatGPT.js
@@ -32,79 +32,74 @@ function ChangeLang(o) {
 }
 
 function Send() {
+    var sQuestion = txtMsg.value;
+    if (sQuestion == "") {
+        alert("Type in your question!");
+        txtMsg.focus();
+        return;
+    }
 
-    var sQuestion = txtMsg.value;
-    if (sQuestion == "") {
-        alert("Type in your question!");
-        txtMsg.focus();
-        return;
-    }
+    var oHttp = new XMLHttpRequest();
+    oHttp.open("POST", "https://api.openai.com/v1/completions");
+    oHttp.setRequestHeader("Accept", "application/json");
+    oHttp.setRequestHeader("Content-Type", "application/json");
+    oHttp.setRequestHeader("Authorization", "Bearer " + OPENAI_API_KEY)
 
-    var oHttp = new XMLHttpRequest();
-    oHttp.open("POST", "https://api.openai.com/v1/completions");
-    oHttp.setRequestHeader("Accept", "application/json");
-    oHttp.setRequestHeader("Content-Type", "application/json");
-    oHttp.setRequestHeader("Authorization", "Bearer " + OPENAI_API_KEY)
+    oHttp.onreadystatechange = function () {
+        if (oHttp.readyState === 4 && oHttp.status === 200) {
+            var oJson = {}
+            if (txtOutput.value != "") txtOutput.value += "\n";
 
-    oHttp.onreadystatechange = function () {
-        if (oHttp.readyState === 4) {
-            //console.log(oHttp.status);
-            var oJson = {}
-            if (txtOutput.value != "") txtOutput.value += "\n";
+            try {
+                oJson = JSON.parse(oHttp.responseText);
+            } catch (ex) {
+                txtOutput.value += "Error: " + ex.message
+            }
 
-            try {
-                oJson = JSON.parse(oHttp.responseText);
-            } catch (ex) {
-                txtOutput.value += "Error: " + ex.message
-            }
+            if (oJson.error && oJson.error.message) {
+                txtOutput.value += "Error: " + oJson.error.message;
+            } else if (oJson.choices && oJson.choices[0].text) {
+                var s = oJson.choices[0].text;
 
-            if (oJson.error && oJson.error.message) {
-                txtOutput.value += "Error: " + oJson.error.message;
-            } else if (oJson.choices && oJson.choices[0].text) {
-                var s = oJson.choices[0].text;
+                if (selLang.value != "en-US") {
+                    var a = s.split("?\n");
+                    if (a.length == 2) {
+                        s = a[1];
+                    }
+                }
 
-                if (selLang.value != "en-US") {
-                    var a = s.split("?\n");
-                    if (a.length == 2) {
-                        s = a[1];
-                    }
-                }
+                if (s == "") s = "No response";
+                txtOutput.value += "Chat GPT: " + s;
+                TextToSpeech(s);
+            }
+        } else if (oHttp.readyState === 4) {
+            txtOutput.value += "Error: " + oHttp.status;
+        }
+    };
 
-                if (s == "") s = "No response";
-                txtOutput.value += "Chat GPT: " + s;
-                TextToSpeech(s);
-            }            
-        }
-    };
+    var sModel = selModel.value;
+    var iMaxTokens = 2048;
+    var sUserId = "1";
+    var dTemperature = 0.5;
 
-    var sModel = selModel.value;// "text-davinci-003";
-    var iMaxTokens = 2048;
-    var sUserId = "1";
-    var dTemperature = 0.5;    
+    var data = {
+        model: sModel,
+        prompt: sQuestion,
+        max_tokens: iMaxTokens,
+        user: sUserId,
+        temperature: dTemperature,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+        stop: ["#", ";"]
+    }
 
-    var data = {
-        model: sModel,
-        prompt: sQuestion,
-        max_tokens: iMaxTokens,
-        user: sUserId,
-        temperature:  dTemperature,
-        frequency_penalty: 0.0, //Number between -2.0 and 2.0  
-                                //Positive values decrease the model's likelihood 
-                                //to repeat the same line verbatim.
-        presence_penalty: 0.0,  //Number between -2.0 and 2.0. 
-                                //Positive values increase the model's likelihood 
-                                //to talk about new topics.
-        stop: ["#", ";"]        //Up to 4 sequences where the API will stop 
-                                //generating further tokens. The returned text 
-                                //will not contain the stop sequence.
-    }
+    oHttp.send(JSON.stringify(data));
 
-    oHttp.send(JSON.stringify(data));
-
-    if (txtOutput.value != "") txtOutput.value += "\n";
-    txtOutput.value += "Me: " + sQuestion;
-    txtMsg.value = "";
+    if (txtOutput.value != "") txtOutput.value += "\n";
+    txtOutput.value += "Me: " + sQuestion;
+    txtMsg.value = "";
 }
+
 
 function TextToSpeech(s) {
     if (bTextToSpeechSupported == false) return;
